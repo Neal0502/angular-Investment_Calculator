fdb11665e53a88f1c79681511b5ff8575c262b54
hao.wang <hao.wang@ulictek.com>
2024-10-09 15:51:30 +0800

    Outputting data in a table
    Outputting data in a table

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,19 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-investment-result',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css'
 })
 export class InvestmentResultComponent {
-@Input() results?: {
-  year: number,
-  interest: number,
-  valueEndOfYear: number,
-  annualInvestment: number,
-  totalInterest: number,
-  totalAmountInvested: number,
-}[];
+  @Input() results?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[];
+  constructor() {
+
+  }
 }
